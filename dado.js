2107696a70a4fec43a1116ba7936160d7b0edc4d
mariaguidoval <mariaaparecidaparreira1980@gmail.com>
2025-03-10 21:11:28 -0300

    maria aparecida

---
+++ dado.js
@@ -0,0 +1,38 @@
+export default[
+    {
+        "dia":"segunda-feira",
+        "cardapio":"feijoada,arroz,farofa,laranja,couve",
+        "img":"img/image 2.png",
+        "alt":"prato feijoada"
+    },
+    {
+        "dia": "terça-feira",
+        "cardapio": "arroz,macarrão,feijão ,salada",
+        "img": "https://www.nidelins.com.br/wp-content/uploads/2014/03/sabor-caseiro5.jpg",
+        "alt": "prato com macarrão"
+    },
+    {
+        "dia": "quarta-feira",
+        "cardapio": "strogonoff de frango, arroz branco ,salada ",
+        "img": "https://scontent.fbel12-1.fna.fbcdn.net/v/t39.30808-6/456787940_3308785696095287_4746615148094582363_n.jpg?",
+        "alt": "prato strogonoff de frango"
+    },
+    {
+        "dia": "quinta-feira",
+        "cardapio": "arroz,angu baiano,feijão,salada",
+        "img": "https://oquetempracomer.com.br/wp-content/uploads/elementor/thumbs/angu-a-baiana-_1_-q3j6vizk33zpgg7g5deseqni2xrk1lkiffagw5aj6k.webp",
+        "alt": "prato de angu baiano"
+    },
+    {
+        "dia": "sexta-feira",
+        "cardapio": "arroz com frango desfiado",
+        "img": "https://img-global.cpcdn.com/recipes/c3b76798dda2eb3d/680x964cq70/foto-principal-da-receita-arroz-de-frango-desfiado.webp",
+        "alt": "prato de arroz com frango"
+    },
+    {
+        "dia": "sábado",
+        "cardapio": "arroz,vaca atolada,feijão,salada",
+        "img": "https://seguiareceita.com/wp-content/uploads/2024/03/Vaca-Atolada-1024x457.jpg",
+        "alt": "prato de vaca atolada"
+    }
+]
